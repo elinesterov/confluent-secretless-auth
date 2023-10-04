af2fea5e88dd9a62307194ee09f9c489b20c718f
Eli Nesterov <eli.nesterov@gmail.com>
2023-10-04 12:24:19 -0700

    add simple proxy

--- spiffe-proxy/go.mod
+++ spiffe-proxy/go.mod
@@ -5,7 +5,10 @@ go 1.21.0
 require github.com/spf13/cobra v1.7.0
 
 require (
+	github.com/Microsoft/go-winio v0.6.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -14,8 +17,17 @@ require (
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/zeebo/errs v1.3.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/grpc v1.55.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
@@ -24,4 +36,6 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.16.0
+	github.com/spiffe/go-spiffe/v2 v2.1.6
+	go.uber.org/zap v1.26.0
 )
--- spiffe-proxy/main.go
+++ spiffe-proxy/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/spiffe/go-spiffe/v2/svid/jwtsvid"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+	"go.uber.org/zap"
 )
 
 type Config struct {
@@ -15,11 +21,63 @@ type Config struct {
 	Port                 int
 }
 
-func getJwtHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("JWT token placeholder"))
+type OAuthResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	// Optional
+	ExpiresIn int64 `json:"expires_in,omitempty"`
+}
+
+func loggingMiddleware(next http.HandlerFunc, logger *zap.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger.Info("Received request",
+			zap.String("method", r.Method),
+			zap.String("url", r.URL.String()),
+			zap.String("remote_addr", r.RemoteAddr),
+		)
+
+		// Call the next handler
+		next(w, r)
+	}
+}
+
+func getJwtHandler(w http.ResponseWriter, r *http.Request, jwtSource *workloadapi.JWTSource, logger *zap.Logger) {
+	audience := "confluent.io"
+	svid, err := jwtSource.FetchJWTSVID(r.Context(), jwtsvid.Params{
+		Audience: audience,
+	})
+	if err != nil {
+		logger.Error("Error fetching JWT SVID", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	expiresIn := int64(time.Until(svid.Expiry).Seconds())
+	oauthResp := OAuthResponse{
+		AccessToken: svid.Marshal(),
+		TokenType:   "Bearer",
+		ExpiresIn:   expiresIn,
+	}
+
+	jsonResp, err := json.Marshal(oauthResp)
+	if err != nil {
+		logger.Error("Error serializing JSON", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResp)
 }
 
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		fmt.Println("Could not create logger:", err)
+		os.Exit(1)
+	}
+	defer logger.Sync()
+
 	var cfg Config
 
 	var rootCmd = &cobra.Command{Use: "spiffe-proxy"}
@@ -44,18 +102,40 @@ func main() {
 		Use:   "serve",
 		Short: "Start the SPIFFE proxy server",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("Server is starting on port %d\n", cfg.Port)
-			fmt.Printf("Using SPIFFE Workload API socket path: %s\n", cfg.SpiffeEndpointSocket)
+			logger.Info("Starting server", zap.Int("port", cfg.Port), zap.String("spiffe_endpoint_socket", cfg.SpiffeEndpointSocket))
 
-			http.HandleFunc("/api/getjwt", getJwtHandler)
-			http.ListenAndServe(":"+strconv.Itoa(cfg.Port), nil)
+			if _, err := os.Stat(cfg.SpiffeEndpointSocket); os.IsNotExist(err) {
+				logger.Fatal("SPIFFE Workload API socket does not exist", zap.String("path", cfg.SpiffeEndpointSocket))
+			}
+
+			// Initialize the Workload API client
+			client, err := workloadapi.New(context.Background(), workloadapi.WithAddr("unix://"+cfg.SpiffeEndpointSocket))
+			if err != nil {
+				logger.Fatal("Could not connect to SPIFFE Workload API", zap.Error(err))
+			}
+			defer client.Close()
+
+			// JWT Source is a blocking call
+			jwtSource, err := workloadapi.NewJWTSource(context.Background(), workloadapi.WithClient(client))
+			if err != nil {
+				logger.Fatal("Could not create JWT source", zap.Error(err))
+			}
+			defer jwtSource.Close()
+
+			http.HandleFunc("/api/getjwt", loggingMiddleware(func(w http.ResponseWriter, r *http.Request) {
+				getJwtHandler(w, r, jwtSource, logger)
+			}, logger))
+
+			err = http.ListenAndServe(":"+strconv.Itoa(cfg.Port), nil)
+			if err != nil {
+				logger.Fatal("Failed to start HTTP server", zap.Error(err))
+			}
 		},
 	}
 
 	rootCmd.AddCommand(serverCmd)
 
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println("Error executing root command:", err)
-		os.Exit(1)
+		logger.Fatal("Error executing root command", zap.Error(err))
 	}
 }
